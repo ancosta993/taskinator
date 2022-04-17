eb9b402c082f878eb6589ce14b21597d8a7e72e7
Anthony Dcosta <adcosta2000@gmail.com>
2022-04-17 14:50:47 -0400

    dynamically add HTMl elements with value from form inputs by apeending that value in aanother HTML element

--- asset/js/script.js
+++ asset/js/script.js
@@ -5,9 +5,20 @@ var createTaskHandler = function(event){
 
    event.preventDefault();
 
+   var taskNameInput = document.querySelector("input[name='task-name']").value;
+   var taskTypeInput = document.querySelector("select[name='task-type']").value;
+   // create list items
    var listItemEl  = document.createElement("li");
    listItemEl.className = "task-item";
-   listItemEl.textContent = "task-name";
+
+   // create div to hold task info and add to list item
+   var taskInfoEl = document.createElement("div");
+   // give it a class name
+   taskInfoEl.className = "task-info";
+   // add HTML content to div
+   taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3> <span class = 'task-type'>" + taskTypeInput + "</span>";
+
+   listItemEl.appendChild(taskInfoEl);
    taskToDoEl.appendChild(listItemEl);
 };
 
